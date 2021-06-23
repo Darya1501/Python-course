eb88c96b9e35494301baa1c2ddb1b99b346177a2
Darya1501 <72653101+Darya1501@users.noreply.github.com>
2021-06-23 22:27:29 +0300

    Update Bulls_and_cows.py

--- lesson-17/Bulls_and_cows.py
+++ lesson-17/Bulls_and_cows.py
@@ -3,12 +3,10 @@ import random
 def generate_secret_word():
     digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     secret_word = ''
-    
     for i in range(4):
         random_index = random.randint(0, len(digits)-1)
         secret_word += str(digits[random_index])
-        digits.pop(random_index)
-        
+        digits.pop(random_index)    
     return secret_word
 
 
