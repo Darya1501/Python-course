727e8a7adbfb58ae65153058d07a311da85086ad
Darya1501 <72653101+Darya1501@users.noreply.github.com>
2021-06-24 18:18:25 +0300

    Add files via upload

--- lesson-18/My-Tester.py
+++ lesson-18/My-Tester.py
@@ -1,25 +1,54 @@
+import random
+
 questions = ['Сколько будет два плюс два умноженное на два?', 'Бревно нужно распилить на 10 частей, сколько надо сделать распилов?', 'На двух руках 10 пальцев. Сколько пальцев на 5 руках?', 'Укол делают каждые полчаса, сколько нужно минут для трех уколов?', 'Пять свечей горело, две потухли. Сколько свечей осталось?']
 
 answers = [6, 9, 25, 60, 2]
 
 results = ['Идиот', 'Кретин', 'Дурак', 'Нормальный', 'Талант', 'Гений']
 
-count_right_answers = 0
 
-name = input('Введите ваше имя: ')
-
-for i in range(len(questions)):
+def play():
+    count_right_answers = 0    
+    name = input('Введите ваше имя: ')
     
-    print(i+1, 'вопрос:', questions[i])
-    user_answer = int(input())
-    
-    right_answer = answers[i]
-    
-    if user_answer == right_answer:
-        count_right_answers += 1
+    for i in range(len(questions)):
+        random_index = random.randint(0, len(questions)-1)
         
-print('Правильных ответов:', count_right_answers)
-print('Вы,', name, '-', results[count_right_answers])
+        print(i+1, 'вопрос:', questions[random_index])
+        user_answer = input()
+        
+        if user_answer.isdigit():
+            user_answer = int(user_answer)
+        right_answer = answers[random_index]
+        
+        questions.pop(random_index)
+        answers.pop(random_index)
+        
+        if user_answer == right_answer:
+            count_right_answers += 1
+        
+    print('Правильных ответов:', count_right_answers)
+    print('Вы,', name, '-', results[count_right_answers])
 
 
-# Решены задачи 2, 3, 4
+while True:
+    play()
+    
+    play_again = input('Хотите пройти тест ещё раз? ').lower()
+    
+    if play_again == 'да':
+        questions = ['Сколько будет два плюс два умноженное на два?', 'Бревно нужно распилить на 10 частей, сколько надо сделать распилов?', 'На двух руках 10 пальцев. Сколько пальцев на 5 руках?', 'Укол делают каждые полчаса, сколько нужно минут для трех уколов?', 'Пять свечей горело, две потухли. Сколько свечей осталось?']
+        answers = [6, 9, 25, 60, 2]        
+        print('Запускаю!')
+        continue
+    
+    elif play_again == 'нет':
+        print('До новых встреч!')
+        break
+    
+    else:
+        print('Неизвестный ответ, тестирование преращено')
+        break    
+
+
+# Решены задачи 1, 2, 3, 4, 5
